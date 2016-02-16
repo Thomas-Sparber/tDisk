c2396dabd8bdb36a83b7664545b8b854af567aa2
Thomas Sparber <thomas@sparber.eu>
2016-02-16 11:37:07 +0100

    Improved deviceadvisor

--- frontend/include/deviceadvisor.hpp
+++ frontend/include/deviceadvisor.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 
+#include <frontend.hpp>
 #include <resultformatter.hpp>
 
 namespace td
@@ -18,8 +19,9 @@ struct device_type
 	const static device_type invalid;
 
 	const static device_type blockdevice;
-	const static device_type partition;
+	const static device_type blockdevice_part;
 	const static device_type file;
+	const static device_type file_part;
 	const static device_type raid1;
 	const static device_type raid5;
 	const static device_type raid6;
@@ -76,6 +78,39 @@ struct device
 		return true;
 	}
 
+	device getSplitted(uint64_t newsize) const
+	{
+		device d(*this);
+		d.size = newsize;
+
+		if(type == device_type::file || type == device_type::file_part)
+			d.type = device_type::file_part;
+		else if(type == device_type::blockdevice || type == device_type::blockdevice_part)
+			d.type = device_type::blockdevice_part;
+		else
+			throw FrontendException("Can't split a device of type ", type.getName());
+
+		return std::move(d);
+	}
+
+	bool containsDevice(const std::string &n) const
+	{
+		for(const device &d : subdevices)
+		{
+			if(d.name == n)return true;
+			if(d.containsDevice(n))return true;
+		}
+		return false;
+	}
+
+	bool isRedundant() const
+	{
+		if(type == device_type::raid1)return true;
+		if(type == device_type::raid5)return true;
+		if(type == device_type::raid6)return true;
+		return false;
+	}
+
 	std::string name;
 	device_type type;
 	uint64_t size;
@@ -87,7 +122,9 @@ struct tdisk_advice
 {
 	tdisk_advice() :
 		devices(),
-		recommendation_score()
+		recommendation_score(),
+		redundancy_level(),
+		wasted_space()
 	{}
 
 	bool operator== (const tdisk_advice &other)
@@ -100,8 +137,16 @@ struct tdisk_advice
 		return true;
 	}
 
+	bool operator< (const tdisk_advice &other) const
+	{
+		if(recommendation_score == other.recommendation_score)return wasted_space > other.wasted_space;
+		return recommendation_score < other.recommendation_score;
+	}
+
 	std::vector<device> devices;
 	int recommendation_score;
+	int redundancy_level;
+	uint64_t wasted_space;
 }; //end struct tdisk_advice
 
 std::vector<tdisk_advice> getTDiskAdvices(const std::vector<std::string> &files);
@@ -140,13 +185,17 @@ template <> inline std::string createResultString(const advisor::tdisk_advice &d
 		return concat(
 			"{\n",
 				std::vector<char>(hierarchy+1, '\t'), CREATE_RESULT_STRING_MEMBER_JSON(disk, devices, hierarchy+1, outputFormat), ",\n",
-				std::vector<char>(hierarchy+1, '\t'), CREATE_RESULT_STRING_MEMBER_JSON(disk, recommendation_score, hierarchy+1, outputFormat), "\n",
+				std::vector<char>(hierarchy+1, '\t'), CREATE_RESULT_STRING_MEMBER_JSON(disk, recommendation_score, hierarchy+1, outputFormat), ",\n",
+				std::vector<char>(hierarchy+1, '\t'), CREATE_RESULT_STRING_MEMBER_JSON(disk, redundancy_level, hierarchy+1, outputFormat), ",\n",
+				std::vector<char>(hierarchy+1, '\t'), CREATE_RESULT_STRING_MEMBER_JSON(disk, wasted_space, hierarchy+1, outputFormat), "\n",
 			std::vector<char>(hierarchy, '\t'), "}"
 		);
 	else if(outputFormat == "text")
 		return concat(
 				createResultString(disk.devices, hierarchy+1, outputFormat), "\n",
-				CREATE_RESULT_STRING_MEMBER_TEXT(disk, recommendation_score, hierarchy+1, outputFormat), "\n"
+				CREATE_RESULT_STRING_MEMBER_TEXT(disk, recommendation_score, hierarchy+1, outputFormat), "\n",
+				CREATE_RESULT_STRING_MEMBER_TEXT(disk, redundancy_level, hierarchy+1, outputFormat), "\n",
+				CREATE_RESULT_STRING_MEMBER_TEXT(disk, wasted_space, hierarchy+1, outputFormat), "\n"
 		);
 	else
 		throw FormatException("Invalid output-format ", outputFormat);
--- frontend/src/deviceadvisor.cpp
+++ frontend/src/deviceadvisor.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <linux/fs.h>
 #include <list>
-#include <queue>
+#include <set>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
@@ -16,7 +16,7 @@
 using std::cout;
 using std::endl;
 using std::list;
-using std::priority_queue;
+using std::multiset;
 using std::string;
 using std::vector;
 
@@ -30,8 +30,9 @@ int getNextNumber()
 
 const advisor::device_type advisor::device_type::invalid("invalid", getNextNumber());
 const advisor::device_type advisor::device_type::blockdevice("blockdevice", getNextNumber());
-const advisor::device_type advisor::device_type::partition("partition", getNextNumber());
+const advisor::device_type advisor::device_type::blockdevice_part("blockdevice_part", getNextNumber());
 const advisor::device_type advisor::device_type::file("file", getNextNumber());
+const advisor::device_type advisor::device_type::file_part("file_part", getNextNumber());
 const advisor::device_type advisor::device_type::raid1("raid1", getNextNumber());
 const advisor::device_type advisor::device_type::raid5("raid5", getNextNumber());
 const advisor::device_type advisor::device_type::raid6("raid6", getNextNumber());
@@ -39,9 +40,10 @@ const advisor::device_type advisor::device_type::raid6("raid6", getNextNumber())
 struct device_combination
 {
 	device_combination(const vector<advisor::device> *v_devices) :
-		processed(),
 		available(),
 		score(),
+		redundancy_level(),
+		wasted_space(),
 		devices(v_devices)
 	{
 		for(std::size_t i = 0; i < devices->size(); ++i)
@@ -55,24 +57,61 @@ struct device_combination
 	int getRecommendationSum() const
 	{
 		int sum = 0;
-		for(std::size_t index : processed)
+		for(std::size_t index : available)
 			sum += (*devices)[index].recommendation_score;
 		return sum;
 	}
 
 	bool operator< (const device_combination &other) const
 	{
+		if(score == other.score)return wasted_space > other.wasted_space;
 		return score < other.score;
 	}
 
 	bool operator== (const device_combination &other) const
 	{
-		return processed == other.processed;
+		return available == other.available;
 	}
 
-	list<std::size_t> processed;
+	void evaluateScore()
+	{
+		int splitted_count = 0;
+		redundancy_level = 100000;
+		wasted_space = 0;
+
+		for(std::size_t index : available)
+		{
+			const advisor::device &device = (*devices)[index];
+
+			uint64_t minSpace = uint64_t(-1);
+			for(const advisor::device &d : device.subdevices)
+			{
+				minSpace = std::min(minSpace, d.size);
+				if(d.type == advisor::device_type::file_part)splitted_count++;
+				if(d.type == advisor::device_type::blockdevice_part)splitted_count++;
+			}
+
+			for(const advisor::device &d : device.subdevices)
+				wasted_space += d.size - minSpace;
+
+			if(device.type == advisor::device_type::raid6)redundancy_level = std::min(redundancy_level, 2);
+			else if(device.type == advisor::device_type::raid5)redundancy_level = std::min(redundancy_level, 1);
+			else if(device.type == advisor::device_type::raid1)redundancy_level = std::min(redundancy_level, (int)device.subdevices.size()-1);
+			else redundancy_level = 0;
+		}
+
+		int wastedInt = (int)(wasted_space/(1024*1024));
+
+		score =
+			-wastedInt						//Wasted space has negative impact
+			+wastedInt/3*redundancy_level	//Redundancy level has positive impact
+			-wastedInt/6*splitted_count;	//Amount of splitted disks has negative impact
+	}
+
 	list<std::size_t> available;
 	int score;
+	int redundancy_level;
+	uint64_t wasted_space;
 	const vector<advisor::device> *devices;
 }; //end struct device_combination
 
@@ -117,6 +156,31 @@ std::size_t addDevice(const advisor::device &d, vector<advisor::device> &devices
 	return devices.size() - 1;
 }
 
+void createNewRaid(const device_combination &c, multiset<device_combination> &combinations, vector<advisor::device> &devices, advisor::device_type raidType, unsigned int minDevices)
+{
+	if(c.available.size() >= minDevices)
+	{
+		for(std::size_t index : c.available)
+		{
+			//Not creating raid devices of raid devices
+			if(devices[index].type == advisor::device_type::raid6)continue;
+			if(devices[index].type == advisor::device_type::raid5)continue;
+			if(devices[index].type == advisor::device_type::raid1)continue;
+
+			device_combination new_c(c);
+			new_c.available.remove(index);
+
+			advisor::device d;
+			d.type = raidType;
+			d.subdevices.push_back(devices[index]);
+			index = addDevice(d, devices);
+
+			new_c.available.push_back(index);
+			combinations.insert(new_c);
+		}
+	}
+}
+
 vector<advisor::tdisk_advice> advisor::getTDiskAdvices(const vector<string> &files)
 {
 	vector<tdisk_advice> advices;
@@ -127,117 +191,84 @@ vector<advisor::tdisk_advice> advisor::getTDiskAdvices(const vector<string> &fil
 		devices.push_back(getDevice(name));
 	}
 
-	priority_queue<device_combination> combinations;
-	combinations.emplace(&devices);
+	multiset<device_combination> combinations;
+	combinations.insert(device_combination(&devices));
 
 	while(!combinations.empty())
 	{
-		const device_combination c = combinations.top();
-		combinations.pop();
+		const device_combination c = *combinations.begin();
+		combinations.erase(combinations.begin());
 
-		if(c.available.size() >= 4)
-		{
-			//Create Raid6
-			for(std::size_t index : c.available)
-			{
-				//Not creating raid devices of raid devices
-				if(devices[index].type == device_type::raid6)continue;
-				if(devices[index].type == device_type::raid5)continue;
-				if(devices[index].type == device_type::raid1)continue;
-
-				device_combination new_c(c);
-				new_c.processed.push_back(index);
-				new_c.available.remove(index);
-
-				device d;
-				d.type = device_type::raid6;
-				d.subdevices.push_back(devices[index]);
-				index = addDevice(d, devices);
-
-				new_c.available.push_back(index);
-				combinations.push(new_c);
-			}
-		}
-
-		if(c.available.size() >= 3)
-		{
-			//Create Raid5
-			for(std::size_t index : c.available)
-			{
-				//Not creating raid devices of raid devices
-				if(devices[index].type == device_type::raid6)continue;
-				if(devices[index].type == device_type::raid5)continue;
-				if(devices[index].type == device_type::raid1)continue;
-
-				device_combination new_c(c);
-				new_c.processed.push_back(index);
-				new_c.available.remove(index);
-
-				device d;
-				d.type = device_type::raid5;
-				d.subdevices.push_back(devices[index]);
-				index = addDevice(d, devices);
-
-				new_c.available.push_back(index);
-				combinations.push(new_c);
-			}
-		}
-
-		if(c.available.size() >= 2)
-		{
-			//Create Raid1
-			for(std::size_t index : c.available)
-			{
-				//Not creating raid devices of raid devices
-				if(devices[index].type == device_type::raid6)continue;
-				if(devices[index].type == device_type::raid5)continue;
-				if(devices[index].type == device_type::raid1)continue;
-
-				device_combination new_c(c);
-				new_c.processed.push_back(index);
-				new_c.available.remove(index);
-
-				device d;
-				d.type = device_type::raid1;
-				d.subdevices.push_back(devices[index]);
-				index = addDevice(d, devices);
-
-				new_c.available.push_back(index);
-				combinations.push(new_c);
-			}
-		}
+		createNewRaid(c, combinations, devices, advisor::device_type::raid6, 4);
+		createNewRaid(c, combinations, devices, advisor::device_type::raid5, 3);
+		createNewRaid(c, combinations, devices, advisor::device_type::raid1, 2);
 
 		//Finish raid devices
-		for(std::size_t index : c.available)
+		for(const std::size_t index : c.available)
 		{
 			if(devices[index].type != device_type::raid6 &&
 				devices[index].type != device_type::raid5 &&
 				devices[index].type != device_type::raid1)continue;
 
-			for(std::size_t new_index : c.available)
+			for(const std::size_t new_index : c.available)
 			{
 				//Not creating raid devices of raid devices
 				if(devices[new_index].type == device_type::raid6)continue;
 				if(devices[new_index].type == device_type::raid5)continue;
 				if(devices[new_index].type == device_type::raid1)continue;
 
+				if(devices[index].containsDevice(devices[new_index].name))continue;
+
 				device_combination new_c(c);
-				new_c.processed.push_back(index);
-				new_c.processed.push_back(new_index);
 				new_c.available.remove(index);
 				new_c.available.remove(new_index);
 
 				device d = devices[index];
 				d.subdevices.push_back(devices[new_index]);
-				new_index = addDevice(d, devices);
+				std::size_t new_device = addDevice(d, devices);
 
-				new_c.available.push_back(new_index);
-				combinations.push(new_c);
+				new_c.available.push_back(new_device);
+				new_c.evaluateScore();
+				combinations.insert(new_c);
+
+				//Try to split device
+				uint64_t minSize = uint64_t(-1);
+				for(const device &dev : devices[index].subdevices)
+					minSize = std::min(minSize, dev.size);
+
+				if(minSize < devices[new_index].size)
+				{
+					//new_index-device is larger than smallest subdevice
+					// --> It makes sense to split the device
+					device splitted_a = devices[new_index].getSplitted(minSize);
+					device splitted_b = devices[new_index].getSplitted(devices[new_index].size - minSize);
+
+					addDevice(splitted_a, devices);
+					std::size_t splitted_index_b = addDevice(splitted_b, devices);
+
+					device_combination splitted_c(c);
+					splitted_c.available.remove(index);
+					splitted_c.available.remove(new_index);
+					splitted_c.available.push_back(splitted_index_b);
+
+					device splitted_d = devices[index];
+					splitted_d.subdevices.push_back(splitted_a);
+					std::size_t new_splitted_device = addDevice(splitted_d, devices);
+
+					splitted_c.available.push_back(new_splitted_device);
+					splitted_c.evaluateScore();
+					combinations.insert(splitted_c);
+				}
 			}
 		}
 
+		bool isRedundant = true;
+		bool isNotRedundant = true;
 		bool valid = true;
 		tdisk_advice td;
+		td.recommendation_score = c.score;
+		td.redundancy_level = c.redundancy_level;
+		td.wasted_space = c.wasted_space;
 		for(std::size_t index : c.available)
 		{
 			if(!devices[index].isValid())
@@ -246,18 +277,24 @@ vector<advisor::tdisk_advice> advisor::getTDiskAdvices(const vector<string> &fil
 				break;
 			}
 
+			if(devices[index].isRedundant())isNotRedundant = false;
+			else isRedundant = false;
+
 			td.devices.push_back(devices[index]);
 		}
 
-		if(valid)
+		if(valid && (isNotRedundant || isRedundant))
 		{
 			if(std::find(advices.begin(), advices.end(), td) == advices.end())
 			{
-				cout<<createResultString(td, 0, "json")<<endl;
 				advices.push_back(td);
 			}
 		}
 	}
+
+	std::sort(advices.begin(), advices.end());
+	for(const tdisk_advice &advice : advices)
+		cout<<createResultString(advice, 0, "json")<<endl;
 
 	return std::move(advices);
 }

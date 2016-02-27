e7726ee39515aa09f7c40739e655da8fa27db5c3
Thomas Sparber <thomas@sparber.eu>
2016-02-27 12:34:55 +0100

    Separated performance measurement from the file operations

--- kernel_module/src/tdisk_file.h
+++ kernel_module/src/tdisk_file.h
@@ -8,15 +8,10 @@
 #ifndef TDISK_FILE_H
 #define TDISK_FILE_H
 
-#include <linux/timex.h>
 #include <linux/version.h>
-#include "../include/tdisk/interface.h"
 
-/**
-  * Calculates the amount of measured records-1
-  * @see file_update_performance
- **/
-#define PREVIOUS_RECORDS ((1 << MEASURE_RECORDS_SHIFT) - 1)
+#include "../include/tdisk/interface.h"
+#include "tdisk_performance.h"
 
 /**
   * Returns whether the given file is a tDisk
@@ -65,79 +60,6 @@ inline static int file_flush(struct file *file)
 }
 
 /**
-  * This function updates the performance data
-  * of the given device. Actually it calculates
-  * the average and standard deviation over the
-  * last (1 << MEASURE_RECORDS_SHIFT) requests
- **/
-inline static void file_update_performance(struct file *file, int direction, cycles_t time, struct device_performance *perf)
-{
-	unsigned long diff;
-
-	//time is 0 on platforms which have no cycles measure
-	WARN_ONCE(time == 0, "Your processor doesn't count cycles. Performances values may be wrong!");
-	if(perf == NULL || time == 0)return;
-
-	switch(direction)
-	{
-	case READ:
-		if(unlikely(perf->avg_read_time_cycles == 0))
-		{
-			//Means it was the first read operation of this file.
-			//Assuming that this is the averyge read performance
-			perf->avg_read_time_cycles = time;
-			break;
-		}
-
-		//Avg difference
-		if(perf->avg_read_time_cycles > time)
-			diff = perf->avg_read_time_cycles-time;
-		else diff = time-perf->avg_read_time_cycles;
-
-		//The following lines calculate the following
-		//equation using bitshift for better performance
-		//avg = (avg*(records_count-1) + current_time) / (records_count)
-		perf->avg_read_time_cycles *= PREVIOUS_RECORDS;
-		perf->avg_read_time_cycles += time + perf->mod_avg_read;
-		perf->stdev_read_time_cycles *= PREVIOUS_RECORDS;
-		perf->stdev_read_time_cycles += diff + perf->mod_stdev_read;
-
-		perf->mod_avg_read = perf->avg_read_time_cycles & PREVIOUS_RECORDS;
-		perf->avg_read_time_cycles = perf->avg_read_time_cycles >> MEASURE_RECORDS_SHIFT;
-		perf->mod_stdev_read = perf->stdev_read_time_cycles & PREVIOUS_RECORDS;
-		perf->stdev_read_time_cycles = perf->stdev_read_time_cycles >> MEASURE_RECORDS_SHIFT;
-		break;
-	case WRITE:
-		if(unlikely(perf->avg_write_time_cycles == 0))
-		{
-			//Means it was the first read operation of this file.
-			//Assuming that this is the averyge read performance
-			perf->avg_write_time_cycles = time;
-			break;
-		}
-
-		//Avg difference
-		if(perf->avg_write_time_cycles > time)
-			diff = perf->avg_write_time_cycles-time;
-		else diff = time-perf->avg_write_time_cycles;
-
-		//The following lines calculate the following
-		//equation using bitshift for better performance
-		//avg = (avg*(records_count-1) + current_time) / (records_count)
-		perf->avg_write_time_cycles *= PREVIOUS_RECORDS;
-		perf->avg_write_time_cycles += time + perf->mod_avg_write;
-		perf->stdev_write_time_cycles *= PREVIOUS_RECORDS;
-		perf->stdev_write_time_cycles += diff + perf->mod_stdev_write;
-
-		perf->mod_avg_write = perf->avg_write_time_cycles & PREVIOUS_RECORDS;
-		perf->avg_write_time_cycles = perf->avg_write_time_cycles >> MEASURE_RECORDS_SHIFT;
-		perf->mod_stdev_write = perf->stdev_write_time_cycles & PREVIOUS_RECORDS;
-		perf->stdev_write_time_cycles = perf->stdev_write_time_cycles >> MEASURE_RECORDS_SHIFT;
-		break;
-	}
-}
-
-/**
   * This function writes the given bio_vec to
   * file at the given position. It also measures the performance
  **/
@@ -153,7 +75,7 @@ inline static int file_write_bio_vec(struct file *file, struct bio_vec *bvec, lo
 	file_start_write(file);
 	ret = vfs_iter_write(file, &i, pos);
 	file_end_write(file);
-	file_update_performance(file, WRITE, get_cycles()-time, perf);
+	update_performance(WRITE, get_cycles()-time, perf);
 
 	return ret;
 }
@@ -225,7 +147,7 @@ inline static int file_read_bio_vec(struct file *file, struct bio_vec *bvec, lof
 	time = get_cycles();
 	ret = vfs_iter_read(file, &i, pos);
 	//printk_ratelimited(KERN_DEBUG "tDisk: read start: %llu, end: %llu\n", time, get_cycles());
-	file_update_performance(file, READ, get_cycles()-time, perf);
+	update_performance(READ, get_cycles()-time, perf);
 
 	return ret;
 }

780b06f5ba5537c61bbe1f205d14d60c681b625a
Thomas Sparber <thomas@sparber.eu>
2016-06-11 18:12:56 +0200

    Added new backend project "proxy" which is used to tunnel the webinterface through the serial port

---
+++ backend/proxy/src/main.cpp
@@ -0,0 +1,62 @@
+/**
+  *
+  * tDisk backend share
+  * @author Thomas Sparber (2015-2016)
+  *
+ **/
+
+#include <errno.h>
+#include <error.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+
+#include <curldefinitions.hpp>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::stringstream;
+
+const string endString("<[[[[end-of-serial-request]]]]>\n");
+const string tty("/dev/ttyGS0");
+const string host("http://localhost:8080");
+
+int main(int argc, char *args[])
+{
+	initCurl();
+
+	FILE *fd = fopen(tty.c_str(), "r+");
+	if(!fd)
+	{
+		cout<<"Can't open tty "<<tty<<endl;
+		return 1;
+	}
+
+	char c;
+	stringstream ss;
+	while(true)
+	{
+		fread(&c, 1, 1, fd);
+		//if(!fs)cout<<"Error reading from tty "<<tty<<endl;
+
+		ss.write(&c, 1);
+
+		if(ss.str().length() >= endString.length() && ss.str().compare(ss.str().length() - endString.length(), endString.length(), endString) == 0)
+		{
+			const string request = ss.str().substr(0, ss.str().length() - endString.length());
+			cout<<"New request: "<<request<<endl;
+			ss.str(string());
+
+			long code;
+			string contentType;
+			const string result = getSite(host+request, code, contentType);
+			const string answer = result + endString;
+			cout<<"Result: "<<answer<<endl;
+
+			int res = fwrite(answer.c_str(), 1, answer.length(), fd);
+			if(res <= 0)cout<<"Error writing to tty "<<tty<<": "<<strerror(errno)<<endl;
+		}
+	}
+}

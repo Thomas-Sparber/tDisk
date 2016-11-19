0b9b97139fad75b0472af18949e34b666e3c2fbe
Thomas Sparber <thomas@sparber.eu>
2016-11-19 12:45:00 +0000

    Optimized test_access_count.sh script

--- test_access_count.sh
+++ test_access_count.sh
@@ -1,1 +1,8 @@
-for number in {0..100000}; do sudo dd if=/dev/zero of=/media/cdrom0/testfile bs=512 count=1; echo 59742; echo 3 | sudo tee /proc/sys/vm/drop_caches; done
+#!/bin/bash
+
+for number in {0..100000}
+do
+	sudo dd if=/dev/zero of=/media/tDisk/td0/data/testfile bs=512 count=1
+	echo $number
+	echo 3 | sudo tee /proc/sys/vm/drop_caches
+done

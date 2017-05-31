d5dbb318285f337652604264237400a06d6bb6b8
Chris <s.chriz@live.com>
2017-05-31 21:50:48 +0200

    update

--- backend/login_usb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-tdisk_shell_exit() {
-	echo Goodbye
-}
-
-
-trap tdisk_shell_exit EXIT
--- test_access_count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for number in {0..100000}
-do
-	sudo dd if=/dev/zero of=/media/tDisk/td0/data/testfile bs=512 count=1
-	echo $number
-	echo 3 | sudo tee /proc/sys/vm/drop_caches
-done
